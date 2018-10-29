bdd25b65e599481ffcf73d88cfed61105fe36bf7
Mateusz Skrok <m.p.skrok@gmail.com>
2018-10-29 11:55:47 +0100

    changed linking local -> global

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module OceneoApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3001'
+        origins 'http://localhost:3001','https://oceneo.herokuapp.com/'
         resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
