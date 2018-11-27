29f7eba1bb0af67884ca8be9e6614755c7a862ec
Mateusz Skrok <m.p.skrok@gmail.com>
2018-11-27 16:22:59 +0100

    migration modification

--- db/migrate/20181028152335_create_rates.rb
+++ db/migrate/20181028152335_create_rates.rb
@@ -3,8 +3,8 @@ class CreateRates < ActiveRecord::Migration[5.2]
     create_table :rates do |t|
       t.integer :value
       t.text :comment
-      t.references :user_id, foreign_key: true
-      t.references :product_id, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :product, foreign_key: true
 
       t.timestamps
     end
